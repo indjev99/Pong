367f375d825a1c22c7b4e1c44a6e1e452c462d5f
Emil Indzhev <emil.indjev@gmail.com>
2019-03-21 21:43:22 +0200

    Update

--- sources/colours.cpp
+++ sources/colours.cpp
@@ -4,6 +4,6 @@ const double BACKGROUND_COLOUR_R=0;
 const double BACKGROUND_COLOUR_G=0;
 const double BACKGROUND_COLOUR_B=0;
 
-const double FOREGROUND_COLOUR_R=1;
+const double FOREGROUND_COLOUR_R=0.1;
 const double FOREGROUND_COLOUR_G=1;
-const double FOREGROUND_COLOUR_B=1;
+const double FOREGROUND_COLOUR_B=0.1;
